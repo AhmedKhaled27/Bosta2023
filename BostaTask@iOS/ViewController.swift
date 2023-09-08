4a2c77f303b197b07739546cff48160f99e3d3e4
Ahmed Khaled <ahmmkhaled20@gmail.com>
2023-09-08 20:12:47 +0300

    Initial Commit

---
+++ BostaTask@iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BostaTask@iOS
+//
+//  Created by Ahmed Khaled on 08/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
